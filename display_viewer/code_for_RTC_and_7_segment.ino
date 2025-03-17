df87bfc00a51f4c1f3d2e7b3520906cd82360f8b
Eric Grahn <eric.grahn@chasacademy.se>
2025-03-17 14:19:19 +0100

    added test for static and blinking colon

--- display_viewer/code_for_RTC_and_7_segment.ino
+++ display_viewer/code_for_RTC_and_7_segment.ino
@@ -8,6 +8,8 @@
 RTC_DS1307 rtc;
 TM1637Display display(CLK, DIO);
 
+bool alarmStatus = false;
+
 void setup() {
   Serial.begin(9600);
   delay(3000);
@@ -31,6 +33,7 @@ void setup() {
     Serial.println("Time set!");
   }
 
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   display.setBrightness(7);  // Set display brightness (0-7)
 }
 
@@ -47,7 +50,16 @@ void loop() {
 
   // Display the current time on the 7-segment display in HH:MM format
   int displayValue = now.hour() * 100 + now.minute();
-  display.showNumberDecEx(displayValue, 0b01000000, true);  // Show HH:MM with colon
 
-  delay(1000);  // Wait for 1 second before updating the display
+  if (alarmStatus == true) {
+    display.showNumberDecEx(displayValue, 0b01000000, true);  // Show HH:MM with colon
+    delay(1000);
+  }
+  else if (alarmStatus == false) {
+    display.showNumberDecEx(displayValue, 0b01000000, true);
+    delay(500);
+    display.showNumberDecEx(displayValue, 0b00000000, true);
+    delay(500);
+  }
 }
+
